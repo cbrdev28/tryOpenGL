345e49f5e785c3f8dd4d09a6dcf12a8fd76a28cb
cbr <cbrdev28@gmail.com>
2022-02-09 15:04:11 -0800

    Update comments & minor fix in script

--- scripts/cbr-clang-format.sh
+++ scripts/cbr-clang-format.sh
@@ -5,7 +5,7 @@ echo "CBR clang-format"
 # From:
 # https://stackoverflow.com/questions/59895/how-can-i-get-the-source-directory-of-a-bash-script-from-within-the-script-itsel
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-REPO_DIR="${SCRIPT_DIR}/../"
+REPO_DIR="${SCRIPT_DIR}/.."
 
 pushd ${REPO_DIR}
 # This command may need to be refactored when adding more files

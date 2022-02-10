b384876f66ecec49f6c8a95a5f25fcf05cd570e2
cbr <cbrdev28@gmail.com>
2022-02-10 13:46:53 -0800

    Copy paste code from tuto to draw rectangle

--- src/simple-tests.h
+++ src/simple-tests.h
@@ -115,6 +115,93 @@ class SimpleTests {
       return -1;
     }
 
+    // Shaders
+    // Vertex shader
+    unsigned int vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader, 1, &SimpleTests::vertexShaderSource, NULL);
+    glCompileShader(vertexShader);
+    // Check for shader compile erros
+    int success;
+    char infoLog[512];
+    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+    if (!success) {
+      glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+      std::cout << "Failed to compile vertex shader:\n" << infoLog << std::endl;
+    }
+    // Fragment shader
+    unsigned int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader, 1, &SimpleTests::fragmentShaderSource, NULL);
+    glCompileShader(fragmentShader);
+    // Check for shader compile erros
+    glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+    if (!success) {
+      glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
+      std::cout << "Failed to compile fragment shader:\n"
+                << infoLog << std::endl;
+    }
+    // Link shaders
+    unsigned int shaderProgram = glCreateProgram();
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+    glLinkProgram(shaderProgram);
+    // Check for linking errors
+    glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+    if (!success) {
+      glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+      std::cout << "Failed to link shaders:\n" << infoLog << std::endl;
+    }
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
+
+    // Set up vertex data (and buffer(s)) and configure vertex attributes
+    float vertices[] = {
+        0.5f,  0.5f,  0.0f,  // top right
+        0.5f,  -0.5f, 0.0f,  // bottom right
+        -0.5f, -0.5f, 0.0f,  // bottom left
+        -0.5f, 0.5f,  0.0f   // top left
+    };
+    unsigned int indices[] = {
+        // note that we start from 0!
+        0, 1, 3,  // first Triangle
+        1, 2, 3   // second Triangle
+    };
+    unsigned int VBO, VAO, EBO;
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
+    glGenBuffers(1, &EBO);
+    // bind the Vertex Array Object first, then bind and set vertex buffer(s),
+    // and then configure vertex attributes(s).
+    glBindVertexArray(VAO);
+
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices,
+                 GL_STATIC_DRAW);
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float),
+                          (void*)0);
+    glEnableVertexAttribArray(0);
+
+    // note that this is allowed, the call to glVertexAttribPointer registered
+    // VBO as the vertex attribute's bound vertex buffer object so afterwards we
+    // can safely unbind
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+    // remember: do NOT unbind the EBO while a VAO is active as the bound
+    // element buffer object IS stored in the VAO; keep the EBO bound.
+    // glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+
+    // You can unbind the VAO afterwards so other VAO calls won't accidentally
+    // modify this VAO, but this rarely happens. Modifying other VAOs requires a
+    // call to glBindVertexArray anyways so we generally don't unbind VAOs (nor
+    // VBOs) when it's not directly necessary.
+    glBindVertexArray(0);
+
+    // uncomment this call to draw in wireframe polygons.
+    // glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+
     auto start = std::chrono::system_clock::now();
     std::cout << "Hello first window" << std::endl;
     auto end = std::chrono::system_clock::now();
@@ -127,13 +214,30 @@ class SimpleTests {
       glClearColor(0.3f, 0.4f, 0.2f, 1.0f);
       glClear(GL_COLOR_BUFFER_BIT);
 
-      end = std::chrono::system_clock::now();
-      elapsed_seconds =
-          std::chrono::duration_cast<std::chrono::seconds>(end - start).count();
+      // draw our first triangle
+      glUseProgram(shaderProgram);
+      glBindVertexArray(VAO);  // seeing as we only have a single VAO there's no
+                               // need to bind it every time, but we'll do so to
+                               // keep things a bit more organized
+      // glDrawArrays(GL_TRIANGLES, 0, 6);
+      glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+      // glBindVertexArray(0); // no need to unbind it every time
 
       glfwSwapBuffers(window);
       glfwPollEvents();
+
+      // End of rendering, for testing we only run it for 3 seconds
+      end = std::chrono::system_clock::now();
+      elapsed_seconds =
+          std::chrono::duration_cast<std::chrono::seconds>(end - start).count();
     }
+
+    // optional: de-allocate all resources once they've outlived their purpose:
+    // ------------------------------------------------------------------------
+    glDeleteVertexArrays(1, &VAO);
+    glDeleteBuffers(1, &VBO);
+    glDeleteBuffers(1, &EBO);
+    glDeleteProgram(shaderProgram);
 
     glfwTerminate();
     return 0;
@@ -157,4 +261,25 @@ class SimpleTests {
       glfwSetWindowShouldClose(window, true);
     }
   }
+
+  /**
+   * Vertex shader source
+   */
+  static constexpr const char* vertexShaderSource =
+      "#version 330 core\n"
+      "layout (location = 0) in vec3 aPos;\n"
+      "void main()\n"
+      "{\n"
+      "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+      "}\0";
+  /**
+   * Fragment shader source
+   */
+  static constexpr const char* fragmentShaderSource =
+      "#version 330 core\n"
+      "out vec4 FragColor;\n"
+      "void main()\n"
+      "{\n"
+      "   FragColor = vec4(0.8f, 0.7f, 0.4f, 1.0f);\n"
+      "}\n\0";
 };

b607abce5ffc7f245f5785f6f556b018e5696fe3
cbr <cbrdev28@gmail.com>
2022-03-11 17:30:15 -0800

    Add translations for projection

--- src/MainAppController/tests/TestBatchRender.cc
+++ src/MainAppController/tests/TestBatchRender.cc
@@ -49,10 +49,8 @@ TestBatchRender::TestBatchRender(const TestContext& ctx) : Test(ctx) {
   glm::mat4 identityMatrix = glm::mat4{1.0F};
   shader_->setUniformMat4("u_model", identityMatrix);
   shader_->setUniformMat4("u_view", identityMatrix);
-  const auto reversedAspectRatio = 1.0F / ctx.windowManager.getAspectRatio().ratio;
-  const auto zoom = zoom_ * 0.1F;
-  shader_->setUniformMat4("u_projection", glm::ortho(-1.0F * zoom, 1.0F * zoom, -reversedAspectRatio * zoom,
-                                                     reversedAspectRatio * zoom, -1.0F, 1.0F));
+
+  this->updateProjection();
 
   texture_ = std::make_unique<Texture>("../res/textures/wall_texture.png");
   texture_->bind(0);
@@ -69,11 +67,7 @@ TestBatchRender::~TestBatchRender() = default;
 void TestBatchRender::onUpdate(float deltaTime) {}
 
 void TestBatchRender::onRender() {
-  shader_->bind();
-  const auto reversedAspectRatio = 1.0F / this->getTestContext().windowManager.getAspectRatio().ratio;
-  const auto zoom = zoom_ * 0.1F;
-  shader_->setUniformMat4("u_projection", glm::ortho(-1.0F * zoom, 1.0F * zoom, -reversedAspectRatio * zoom,
-                                                     reversedAspectRatio * zoom, -1.0F, 1.0F));
+  this->updateProjection();
   GLCall(glClearColor(backgroundColor_[0], backgroundColor_[1], backgroundColor_[2], backgroundColor_[3]));
   renderer_.draw(*shader_, *va_, *ib_);
 }
@@ -84,6 +78,20 @@ void TestBatchRender::onImGuiRender() {
   ImGui::Text("%s", this->getTestContext().windowManager.getAspectRatio().formattedValue().c_str());
   ImGui::ColorEdit4("Color", backgroundColor_.data());
   ImGui::SliderFloat("Zoom", &zoom_, 1.0F, 100.0F, "WIP value = %.2f");
+  ImGui::SliderFloat("Delta X", &deltaX_, -100.0F, 100.0F, "WIP value = %.2f");
+  ImGui::SliderFloat("Delta Y", &deltaY_, -100.0F, 100.0F, "WIP value = %.2f");
+}
+
+void TestBatchRender::updateProjection() {
+  shader_->bind();
+  const auto reversedAspectRatio = 1.0F / this->getTestContext().windowManager.getAspectRatio().ratio;
+  const auto zoom = zoom_ * 0.1F;
+  const auto deltaX = deltaX_ * 0.1F;
+  const auto deltaY = deltaY_ * 0.1F;
+  const auto orthoX = 1.0F * zoom;
+  const auto orthoY = reversedAspectRatio * zoom;
+  shader_->setUniformMat4("u_projection", glm::ortho((-orthoX) + deltaX, orthoX + deltaX, (-orthoY) + deltaY,
+                                                     orthoY + deltaY, -1.0F, 1.0F));
 }
 
 }  // namespace test

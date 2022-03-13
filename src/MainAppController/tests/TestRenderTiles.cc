caf7db9fa8e3490933396ee607dfefd9d2296792
cbr <cbrdev28@gmail.com>
2022-03-13 14:52:06 -0700

    Try to center tile vertices

--- src/MainAppController/tests/TestRenderTiles.cc
+++ src/MainAppController/tests/TestRenderTiles.cc
@@ -107,8 +107,10 @@ auto TestRenderTiles::makeTileVertices(unsigned int count) -> std::vector<TileVe
 
   for (unsigned int i = 0; i < count; i++) {
     for (unsigned int j = 0; j < count; j++) {
-      const auto posX = static_cast<float>(j) * (tileSize + tileSpacing);
-      const auto posY = static_cast<float>(i) * (tileSize + tileSpacing);
+      auto posX = static_cast<float>(j) * (tileSize + tileSpacing);
+      posX = posX - (static_cast<float>(count) / 2.0F);
+      auto posY = static_cast<float>(i) * (tileSize + tileSpacing);
+      posY = posY - (static_cast<float>(count) / 2.0F);
       TileVertex vertex1 = {{posX, posY}, {0.0F, 0.0F}, 0.0F};
       TileVertex vertex2 = {{posX + tileSize, posY}, {1.0F, 0.0F}, 0.0F};
       TileVertex vertex3 = {{posX + tileSize, posY + tileSize}, {1.0F, 1.0F}, 0.0F};

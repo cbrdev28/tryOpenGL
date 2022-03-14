db808d0d977f43ef682c988b752b12366e4b6773
cbr <cbrdev28@gmail.com>
2022-03-13 20:06:10 -0700

    Refactor tile vertex

--- src/MainAppController/common/TileVertex.h
+++ src/MainAppController/common/TileVertex.h
@@ -5,8 +5,8 @@
 #include <vector>
 
 struct TileVertex {
-  static const unsigned int posCount = 2;
-  static const unsigned int textureCoordCount = 2;
+  static constexpr unsigned int posCount = 2;
+  static constexpr unsigned int textureCoordCount = 2;
 
   std::array<float, posCount> positions;
   std::array<float, textureCoordCount> textureCoords;
@@ -15,10 +15,10 @@ struct TileVertex {
   inline static auto serialize(const std::vector<TileVertex>& tileVertices) -> std::vector<float> {
     // The goal is to transform a vector of TileVertex into a vector of float
     // Count how many floats we have in 1 TileVertex (used to reserve vector size)
-    const auto numOfFloatValues = posCount + textureCoordCount + 1;
-
+    const auto numOfFloatValues = posCount + textureCoordCount + 1;  // 1 float for textureIdx
     std::vector<float> serializedTiles = {};
     serializedTiles.reserve(numOfFloatValues * tileVertices.size());
+
     for (auto& tileVertex : tileVertices) {
       serializedTiles.emplace_back(tileVertex.positions[0]);
       serializedTiles.emplace_back(tileVertex.positions[1]);

3726fbd19ef04cff84dec1e45ec045bb5b5a3d46
Charles-Basile <cbrdev28@gmail.com>
2022-04-05 16:22:49 -0700

    Add CPack

---
+++ scripts/cbr-package-release.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -e
+echo "CBR cpack RELEASE"
+
+# From:
+# https://stackoverflow.com/questions/59895/how-can-i-get-the-source-directory-of-a-bash-script-from-within-the-script-itsel
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+REPO_DIR="${SCRIPT_DIR}/.."
+BUILD_DIR="${REPO_DIR}/build-release"
+
+# Run our install script
+pushd ${SCRIPT_DIR}
+./cbr-install-release.sh
+popd
+
+pushd ${BUILD_DIR}
+cpack -C Release
+cpack -G ZIP -C Release
+popd

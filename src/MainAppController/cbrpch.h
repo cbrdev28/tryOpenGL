565793b5fce09094dd3005d517887fdeda343789
Charles-Basile <cbrdev28@gmail.com>
2022-04-06 14:45:30 -0700

    Reformat

--- src/MainAppController/cbrpch.h
+++ src/MainAppController/cbrpch.h
@@ -5,30 +5,29 @@
 #include <Windows.h>
 #endif  // CBR_WINDOWS
 
-#include <memory>
-#include <string>
-#include <array>
-#include <fstream>
-#include <sstream>
-#include <utility>
-#include <unordered_map>
-#include <vector>
-#include <chrono>
-#include <cassert>
-#include <condition_variable>
-#include <functional>
-#include <future>
-#include <mutex>
-#include <queue>
-#include <thread>
-#include <algorithm>
-#include <gsl/gsl>
-
 #include <fmt/core.h>
 
+#include <algorithm>
+#include <array>
+#include <cassert>
+#include <chrono>
+#include <condition_variable>
+#include <fstream>
+#include <functional>
+#include <future>
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
+#include <gsl/gsl>
+#include <memory>
+#include <mutex>
+#include <queue>
+#include <sstream>
+#include <string>
+#include <thread>
+#include <unordered_map>
+#include <utility>
+#include <vector>
 
 // Disable clang-format because we must include glad before GLFW
 // clang-format off
@@ -36,10 +35,9 @@
 #include <GLFW/glfw3.h>
 // clang-format on
 
-#include <stb_image.h>
-
 #include <imgui.h>
 #include <imgui_impl_glfw.h>
 #include <imgui_impl_opengl3.h>
+#include <stb_image.h>
 
 #endif  // CBR_PCH_H_

9ea3debd585d3a9a7bac442bb011e85fd81f1c78
Charles-Basile <cbrdev28@gmail.com>
2022-04-11 15:29:59 -0700

    Add base vertex buffer: vertices & textures

--- src/MainAppController/tests/SceneTraining.cc
+++ src/MainAppController/tests/SceneTraining.cc
@@ -2,6 +2,8 @@
 
 #include "SceneTraining.h"
 
+#include "VertexBufferLayout.h"
+
 namespace test {
 
 SceneTraining::SceneTraining(const TestContext& ctx)
@@ -11,13 +13,38 @@ SceneTraining::SceneTraining(const TestContext& ctx)
   renderer_.enableBlend();
   textures_.at(TextureIdx::MAIN_CHARACTER) = std::make_unique<Texture>(currentCharacter_->texturePath);
 
+  vbBaseVertices_ =
+      std::make_unique<VertexBuffer>(baseSquareModel_.vertices_.data(), sizeof(baseSquareModel_.vertices_));
+  vbBaseTextures_ =
+      std::make_unique<VertexBuffer>(baseSquareModel_.textures_.data(), sizeof(baseSquareModel_.textures_));
+
+  vbBaseVertices_->setDivisor(VertexBufferDivisor::ALWAYS);
+  vbBaseTextures_->setDivisor(VertexBufferDivisor::ALWAYS);
+
+  VertexBufferLayout verticesLayout;
+  verticesLayout.pushFloat(2);  // Each vertex is made of 2 floats
+  VertexBufferLayout texturesLayout;
+  texturesLayout.pushFloat(2);  // Each texture coordinate is made of 2 floats
+
+  va_->setInstanceBufferLayout({
+      {*vbBaseVertices_, verticesLayout},
+      {*vbBaseTextures_, texturesLayout},
+  });
+
   shader_->bind();
+  shader_->setUniformMat4("u_view", glm::mat4(1.0F));
+  const auto& aspectRatio = this->getTestContext().windowManager->getAspectRatio().ratio;
+  shader_->setUniformMat4("u_projection", glm::ortho(-aspectRatio, aspectRatio, -1.0F, 1.0F, -1.0F, 1.0F));
+
   for (unsigned int i = 0; i < textures_.size(); ++i) {
     textures_.at(i)->bind(i);
     shader_->setUniform1i(fmt::format("u_textureSampler_{}", i), static_cast<GLint>(i));
   }
 
   shader_->unBind();
+  va_->unBind();
+  vbBaseTextures_->unBind();
+  vbBaseVertices_->unBind();
 }
 
 SceneTraining::~SceneTraining() = default;
--- src/MainAppController/tests/SceneTraining.h
+++ src/MainAppController/tests/SceneTraining.h
@@ -1,12 +1,16 @@
 #ifndef SCENE_TRAINING_H_
 #define SCENE_TRAINING_H_
 
+// #include "CharacterModel.h"
+#include "BaseSquareModel.h"
 #include "GameCharacter.h"
 #include "GameManager.h"
 #include "Renderer.h"
 #include "Shader.h"
 #include "Test.h"
 #include "Texture.h"
+#include "VertexArray.h"
+#include "VertexBuffer.h"
 
 namespace test {
 
@@ -29,12 +33,18 @@ class SceneTraining : public Test {
 
   Renderer renderer_;
   std::unique_ptr<Shader> shader_{new Shader("res/shaders/scene_training.shader")};
+  std::unique_ptr<VertexArray> va_{new VertexArray};
+  std::unique_ptr<VertexBuffer> vbBaseVertices_;
+  std::unique_ptr<VertexBuffer> vbBaseTextures_;
 
   enum TextureIdx {
     MAIN_CHARACTER = 0,
   };
   static constexpr unsigned int kTexturesCount = 1;
   std::array<std::unique_ptr<Texture>, kTexturesCount> textures_;
+
+  BaseSquareModel baseSquareModel_;
+  // CharacterModel characterModel_;
 };
 
 }  // namespace test
